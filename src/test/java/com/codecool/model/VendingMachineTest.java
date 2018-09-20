1e036b981763da1da3c8640e6a81f0f41b403743
edobkowski <edobkowski@gmail.com>
2018-09-20 13:52:44 +0200

    Add test case: acceptance valid coins

---
+++ src/test/java/com/codecool/model/VendingMachineTest.java
@@ -0,0 +1,39 @@
+package com.codecool.model;
+
+import com.codecool.helpers.CoinTypes;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendingMachineTest {
+
+    private VendingMachine vendingMachine;
+    private static Map<CoinTypes, Integer> initialMachineCoins;
+
+    @BeforeAll
+    static void init() {
+        initialMachineCoins = new HashMap<>();
+        initialMachineCoins.put(CoinTypes.DIME, 20);
+        initialMachineCoins.put(CoinTypes.NICKLE, 20);
+        initialMachineCoins.put(CoinTypes.QUARTER, 20);
+    }
+
+    @BeforeEach
+    void initForTest() {
+        this.vendingMachine = new VendingMachine(initialMachineCoins);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = CoinTypes.class, names = {"DIME", "QUARTER", "NICKLE"})
+    void acceptCoinTest_Accepted(CoinTypes coin) {
+        assertTrue(vendingMachine.acceptCoin(coin));
+    }
+}
