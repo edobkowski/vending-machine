d122002e13b7dd2e0baa995e8175b3124c94d0d7
edobkowski <edobkowski@gmail.com>
2018-09-20 13:51:36 +0200

    Add value attribute

--- src/main/java/com/codecool/helpers/CoinTypes.java
+++ src/main/java/com/codecool/helpers/CoinTypes.java
@@ -1,18 +1,20 @@
 package com.codecool.helpers;
 
 public enum CoinTypes {
-    NICKLE(21.21, 5.0),
-    DIME(17.91, 2.268),
-    QUARTER(24.26, 5.67),
-    PENCE(21.4, 5.0)
+    NICKLE(21.21, 5.0, 0.05),
+    DIME(17.91, 2.268, 0.1),
+    QUARTER(24.26, 5.67, 0.25),
+    PENCE(21.4, 5.0, 0.01)
     ;
 
     private final double size;
     private final double weight;
+    private final double value;
 
-    CoinTypes(double size, double weight) {
+    CoinTypes(double size, double weight, double value) {
         this.size = size;
         this.weight = weight;
+        this.value = value;
     }
 
     public double getSize() {
@@ -21,5 +23,9 @@ public enum CoinTypes {
 
     public double getWeight() {
         return weight;
+    }
+
+    public double getValue() {
+        return value;
     }
 }
